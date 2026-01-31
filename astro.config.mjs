3e678f030c7c28779660fdbf9ebed2ac29983797
cynddl <luc@rocher.lc>
2026-01-31 21:17:11 +0000

    Add asset compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import preact from '@astrojs/preact';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+import playformCompress from '@playform/compress';
+
 export default defineConfig({
   output: 'server',
-  integrations: [preact({ compat: true })],
+  integrations: [preact({ compat: true }), playformCompress()],
 
   vite: {
     plugins: [tailwindcss()],
